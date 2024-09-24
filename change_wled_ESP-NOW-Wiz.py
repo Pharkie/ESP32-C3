9fdffc7e5d12b3ccce8a16aedf537821de632e2f
Adam Knowles <1413836+Pharkie@users.noreply.github.com>
2024-09-24 13:13:17 +0100

    ESP-NOW working

--- change_wled_ESP-NOW-Wiz.py
+++ change_wled_ESP-NOW-Wiz.py
@@ -1,13 +1,12 @@
-# Wizmote code derived from
-# https://github.com/ChuckMash/ESPythoNOW/blob/main/examples/wizmote_send.py#L19-L29
-
 import network
 import espnow
 import struct
 import urandom  # Use urandom for MicroPython
+import os
+import secrets
 
-# MAC for WLED controller (Gled2): ec64c9a78c5c
-wled_mac = b'\xec\x64\xc9\xa7\x8c\x5c'
+wled_mac = secrets.wled_mac
+SEQUENCE_FILE = "sequence.txt"
 
 class WIZmote:
     def __init__(self):
@@ -23,7 +22,7 @@ class WIZmote:
             "+": 9
         }
         self.espnow = self.initialize_espnow()
-        self.sequence = 1
+        self.sequence = self.load_sequence()
 
     def initialize_espnow(self):
         # Initialize Wi-Fi in station mode
@@ -37,6 +36,16 @@ class WIZmote:
         espnow_instance.add_peer(wled_mac)
         return espnow_instance
 
+    def load_sequence(self):
+        if SEQUENCE_FILE in os.listdir():
+            with open(SEQUENCE_FILE, "r") as f:
+                return int(f.read().strip())
+        return 1
+
+    def save_sequence(self):
+        with open(SEQUENCE_FILE, "w") as f:
+            f.write(str(self.sequence))
+
     def send_button(self, button):
         msg = b""
         msg += struct.pack('<B',  0x91 if button == "ON" else 0x81) # Type
@@ -47,6 +56,8 @@ class WIZmote:
         msg += struct.pack('<B',  100)                              # Battery level
         msg += struct.pack('<4B', *[urandom.getrandbits(8) for _ in range(4)])  # Random bytes
 
+        print(f"Sending message with sequence number: {self.sequence}")
+
         try:
             for channel in range(1, 14):  # Iterate through Wi-Fi channels 1 to 13
                 network.WLAN(network.STA_IF).config(channel=channel)
@@ -56,7 +67,8 @@ class WIZmote:
             print("Error sending message:", e)
         
         self.sequence += 1
+        self.save_sequence()
 
 # Test
 wizmote = WIZmote()
-wizmote.send_button("OFF")
+wizmote.send_button("ON")
